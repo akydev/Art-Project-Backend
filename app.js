8340454d991130526ad1a6525442b301bbc0f794
akydev <akydev18@gmail.com>
2025-02-02 23:46:22 +0530

    Server and Database Connection is done.

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-// Import express
-const express = require("express"); //Express module included in this file.
+const express = require("express");
+const { dbConnection } = require("./db/database");
+const userRoutes = require("./routhing/user");
 
-// Create an Express app
-const app = express(); //Now express is intrigated in the app function.
+const app = express();
+const port = 3001;
 
-// const port = 3000;
-// app.listen(() => console.log("Server listening on port number", port));
+app.use(express.json());
 
-// Define a route for the root URL
 app.get("/", (req, res) => {
-  res.send("This is the server response message: Hello World..!");
+  res.send("Hello World!");
 });
 
-app.get("/greet", (req, res) => {
-  const msg = req.query.name;
-  res.send(`Hello ${msg}`);
-});
+app.use("api/user", userRoutes);
 
-app.use(express.json()); // Middleware to parse JSON bodies
+const startConnection = async () => {
+  try {
+    await dbConnection("mongodb://localhost:27017/nodejstest");
+    await app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.post("/submit", (req, res) => {
-  const { username, email } = req.body;
-  res.send(`Received data: ${username}, ${email}`);
-});
-
-// Set the server to listen on port 3000
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+startConnection();
